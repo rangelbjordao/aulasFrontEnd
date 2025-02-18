09fa35f16bc77ff2db032c71d54882641fea68fd
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2025-02-18 09:53:19 -0300

    aula

--- aula-25-18-02/src/app/layout.tsx
+++ aula-25-18-02/src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import Header from "./components/Header/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
