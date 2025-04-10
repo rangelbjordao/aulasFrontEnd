587e7dc52d9c13ca6435544072ab07211d10c1e0
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2025-04-10 13:32:01 -0300

    Update page.tsx

--- 2SEM/cp05-front/src/app/page.tsx
+++ 2SEM/cp05-front/src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
     return (
         <>
             <main>
+                {/* Titulo principal */}
                 <h1 className="my-2 text-center text-3xl font-bold">CP 05</h1>
             </main>
         </>
